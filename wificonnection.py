20e73eaac039a36d69e66b54561fcda4aed46854
cvaram96 <cvaram96@gmail.com>
2020-07-20 22:53:14 +0530

    ArgParser is added

--- wificonnection.py
+++ wificonnection.py
@@ -1,17 +1,39 @@
+import optparse
+
 import qrcode
 
-qr = qrcode.QRCode(version=2,
-                   error_correction=qrcode.constants.ERROR_CORRECT_Q,
-                   box_size=15,
-                   border=5
-                   )
-#Set your SSID Here
 
-ssid = 'siva'
-#Set your password here
-password = 'asfsa'
-data = 'WIFI:S:<{}>;T:<WPA|WEP|>;P:<{}>;H:<true|false|>;'.format(ssid, password)
-qr.add_data(data)
-qr.make(fit=True)
-img =qr.make_image(fill='black',back_color ='white')
-img.save('wifi.png')
+def getArguments():
+  parser = optparse.OptionParser()
+  parser.add_option("-s", "--ssid", dest="ssid", help="Enter your WIFI SSID")
+  parser.add_option("-p", "--password", dest="password",
+                    help="Enter your WIFI password")
+
+  args_options, arguments = parser.parse_args()
+  return args_options
+
+
+def createQRCode(ssid, password):
+  qr = qrcode.QRCode(version=2,
+                     error_correction=qrcode.constants.ERROR_CORRECT_Q,
+                     box_size=15,
+                     border=5
+                     )
+  data = 'WIFI:S:<{}>;T:<WPA|WEP|>;P:<{}>;H:<true|false|>;'.format(ssid,
+                                                                   password)
+  qr.add_data(data)
+  qr.make(fit=True)
+  img = qr.make_image(fill='black', back_color='white')
+  img.save('wifi.png')
+
+
+options = getArguments()
+
+if options.ssid is not None:
+  if options.password is not None:
+    createQRCode(ssid=options.ssid, password=options.password)
+    print('Your QR Code is generated successfully for the {} SSID',format(options.ssid))
+  else:
+    print('Please enter your {} network password'.format(options.ssid))
+else:
+  print('Please Enter Your SSID First...............')
